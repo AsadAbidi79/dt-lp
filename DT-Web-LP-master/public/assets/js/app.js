96974053d31be833f006af630af66de0a2ef7e52
asadabidi79 <asadabidi45@gmail.com>
2023-06-10 23:54:16 +0500

    dt

---
+++ DT-Web-LP-master/public/assets/js/app.js
@@ -0,0 +1,82 @@
+var a = 0;
+$(window).scroll(function () {
+  var oTop = $("#counter-box").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $(".counter").each(function () {
+      var $this = $(this),
+        countTo = $this.attr("data-number");
+      $({
+        countNum: $this.text()
+      }).animate(
+        {
+          countNum: countTo
+        },
+
+        {
+          duration: 850,
+          easing: "swing",
+          step: function () {
+            //$this.text(Math.ceil(this.countNum));
+            $this.text(
+              Math.ceil(this.countNum).toLocaleString("en")
+            );
+          },
+          complete: function () {
+            $this.text(
+              Math.ceil(this.countNum).toLocaleString("en")
+            );
+            //alert('finished');
+          }
+        }
+      );
+    });
+    a = 1;
+  }
+});
+
+//////////// Navbar Background Change
+// $(function () {
+//   $(document).scroll(function () {
+//     var $nav = $(".fixed-top");
+//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//   });
+// });
+
+///////// Navbar disappear while scrolling
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//     document.getElementById("navbar").style.transition = "500ms all";
+//   } else {
+//     document.getElementById("navbar").style.top = "-1500px";
+//     document.getElementById("navbar").style.transition = "500ms all";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 40,
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      }
+    }
+  });
+});
